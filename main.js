71bd2dedbe420013c27c55f40bac33d1de55d3bc
Angelos Orfanakos <me@agorf.gr>
2019-08-09 19:45:14 +0300

    Load index.html in main window when app is ready

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow();
+  mainWindow.loadFile('index.html');
+};
+
+app.on('ready', createMainWindow);
