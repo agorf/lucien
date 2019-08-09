244c791bde46b470108c349b036d8af80e97d96b
Angelos Orfanakos <me@agorf.gr>
2019-08-09 23:06:27 +0300

    Render Markdown to HTML on file open

--- src/editor.js
+++ src/editor.js
@@ -1,7 +1,14 @@
 const { ipcRenderer } = require('electron');
+const marked = require('marked');
 
 const markdownView = document.querySelector('.markdown-view');
+const htmlView = document.querySelector('.html-view');
+
+const renderMarkdownToHTML = markdown => {
+  htmlView.innerHTML = marked(markdown);
+};
 
 ipcRenderer.on('file-opened', (event, { data }) => {
   markdownView.value = data;
+  renderMarkdownToHTML(data);
 });
